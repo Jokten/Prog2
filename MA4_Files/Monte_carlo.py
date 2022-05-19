d30838853009c0596829992ca1398eb0b6860c81
Jokten123 <Jokten123@gmail.com>
2022-05-19 23:26:19 +0200

    jg

--- MA4_Files/Monte_carlo.py
+++ MA4_Files/Monte_carlo.py
@@ -1,11 +1,11 @@
 import random
-import matplotlib
+import matplotlib.pyplot as plt
 in_circle = 0
 redx = []
 redy = []
 bluex = []
 bluey = [] 
-n = 1000000
+n = 100000
 for i in range(n):
     x = random.uniform(-1,1)
     y = random.uniform(-1,1)
@@ -13,14 +13,20 @@ for i in range(n):
 
     if x**2 + y**2 <= 1:
         in_circle += 1
-"""""
         redx.append(x)
         redy.append(y)
     else:
         bluex.append(x)
         bluey.append(y)
+
+plt.plot(redx,redy, 'bo')
+plt.plot(bluex,bluey, 'ro')
+plt.savefig('pi100000.png')
+plt.show()
+
+print(4*in_circle/n) 
 """""
-#plt.plot(redx,redy, 'bo')
-#plt.plot(bluex,bluey, 'ro')
-#plt.show(block=True)
-#print(4*in_circle/n) 
+n = 1000 --> pi = 3.092
+n = 10000 --> pi = 3.1488
+n = 100000 --> pi = 3.13964
+"""""
