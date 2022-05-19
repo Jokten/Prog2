a8ffaf3d2b0f0033ae4ceed235079d0ad1d3ac78
Jokten123 <Jokten123@gmail.com>
2022-05-19 23:37:31 +0200

    o

--- MA4_Files/MA4_2.py
+++ MA4_Files/MA4_2.py
@@ -1,25 +1,35 @@
 #!/usr/bin/env python3.9
+from cProfile import label
 from time import perf_counter as pc
 from person import Person
 from numba import njit
+import matplotlib.pyplot as plt
 def main():
-	f = Person(5)
-	print(f.get())
-	f.set(4)
-	print(f.fib())
-	start = pc()
-	fib_py(17)
-	end = pc()
+	py = []
+	num = []
+	c = []
+	for i in range(30, 46):
+		start = pc()
+		fib_py(i)
+		end = pc()
+		fib2_py(i)
+		end2 = pc()
+		f = Person(i)
+		f.fib()
+		end3 = pc()
+		py.append(end-start)
+		num.append(end2-end)
+		c.append(end3-end2)
+	x = list(range(30,46))
+	plt.plot(x,py,label='py')
+	plt.plot(x,num,label='numba')
+	plt.plasma(x,c,label='c')
+	plt.savefig('time.png')
+	plt.show()
 	print(fib2_py(47))
-	end2 = pc()
-	f = Person(47)
-	print(f.fib())
-	end3 = pc()
-
-	print(end-start)
-	print(end2-end)
-	print(end3-end2)
-
+	ff = Person(47)
+	print(ff.fib())
+	
 def fib_py(n):
 	if n <= 1:
 		return n
